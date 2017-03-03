bd7630604219bff8f38213ea3a93396b3251c7f0
Vidit Chopra <viditchopra@outlook.com>
2017-03-03 13:43:54 -0800

    Created App, with Button

--- ButtonApp/ViewController.swift
+++ ButtonApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BackGround: UIImageView!
+    
+    @IBOutlet weak var Hello: UIImageView!
+    
+    @IBOutlet weak var btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnPress(_ sender: Any) {
+        
+        Hello.isHidden = false
+        BackGround.isHidden = false
+        btn.isHidden = true
+        
+    }
 
 }
 
